fc01508f19e2ae3cb2d943345ac81cfaea012048
Kane Baccigalupi <Kane.Baccigalupi@meraki.net>
2023-02-28 19:39:52 -0800

    Adds more tests and code

--- test/calculate.test.js
+++ test/calculate.test.js
@@ -19,10 +19,77 @@ describe("calculate", () => {
   });
 
   describe("branch", () => {
-    it("counts a ternary with no other logic as a 3", () => {
+    it("penalizes a ternary with an extra branch score", () => {
       // ternary operations seem succinct, but are very hard to read. This adds
       // a 1 point penalty in addation to the two branches.
       expect(calculate("a ? b : c;")).toEqual(3);
+    });
+
+    it("counts an if with only one block as 1 point", () => {
+      expect(calculate("if (a) { b; }")).toEqual(1);
+    });
+
+    it("counts an if/else as a 2", () => {
+      expect(calculate(`
+        if (a) { b; }
+        else { c; }
+      `)).toEqual(2);
+    });
+
+    it("counts a multi-step if/else as a number for each block and one for each extra if", () => {
+      expect(calculate(`
+        if (a) {
+          b;
+        }
+        else if (c) {
+          d;
+        }
+        else if (e) {
+          f;
+        }
+        else {
+          g;
+        }
+      `)).toEqual(6);
+    });
+
+    it("counts nested if/elses the same way it would could them independently", () => {
+      expect(calculate(`
+        if (a) {
+          if (b) {
+            c;
+          } else {
+            d;
+          }
+        } else {
+          e;
+        }
+      `)).toEqual(4);
+    });
+
+    it("penalizes labeled statements", () => {
+      expect(calculate(`
+        goHere:
+        a;
+      `)).toEqual(5);
+    });
+
+    it("penalizes break statements", () => {
+      expect(calculate(`
+        while(true) {
+          if (a) {
+            break;
+          }
+        }
+      `)).toEqual(4); // 1 for the while loop, 1 for the conditional, and 2 for the break
+    });
+
+    it("penalizes with statements", () => {
+      expect(calculate(`
+        with(a) {
+          b;
+        }
+      `)).toEqual(5);
     });
   });
 });
